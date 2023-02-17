51c058d78e14f7e2cb0e46ec37aa347d7831f08c
OddBxy <118909861+OddBxy@users.noreply.github.com>
2023-02-17 17:47:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,9 +14,11 @@
 
 #define parametre_PORT "-p"
 #define parametre_SIZE "-s"
+#define parametre_LIMIT "-l"
 
 typedef struct Parametre{
 	int PORT;
+	int limite;
 	int *dimensions;
 }Parametre;
 
@@ -34,6 +36,7 @@ Parametre parametrage(int nbArgs, char *args[]){
 	//si le parametre args est vide, le port et la matrice sont initialiser avec les valeurs suivantes :
 	Parametre parametre;
 	parametre.PORT = 5000;
+	parametre.limite = 10;
 	parametre.dimensions = (int*) calloc(2, sizeof(int));
 	parametre.dimensions[0] = 80;
 	parametre.dimensions[1] = 40;
@@ -49,6 +52,11 @@ Parametre parametrage(int nbArgs, char *args[]){
 			parametre.PORT = atoi(args[i]);
 			//atoi permet de convertir des strings en integers
 		}
+		else if( strcmp(args[i], parametre_LIMIT) == 0){
+			i++;
+			parametre.limite = atoi(args[i]);
+			//atoi permet de convertir des strings en integers
+		}
 		else if( strcmp(args[i], parametre_SIZE) == 0){
 			char *dimensions_string[2] = {"", ""};
 			i++;
@@ -62,7 +70,6 @@ Parametre parametrage(int nbArgs, char *args[]){
 	
 	return parametre;
 }
-
 
 User *ajout_Client(User *liste, int socketEcoute, socklen_t *address_len){
 	struct sockaddr_in pointDeRencontreDistant;
@@ -84,16 +91,65 @@ User *ajout_Client(User *liste, int socketEcoute, socklen_t *address_len){
 	return liste;
 }
 
+
+void reception_message(struct pollfd *poll_message, int nbClient){
+
+	char messageEnvoi[LG_MESSAGE]; /* le message de la couche Application ! */
+	char messageRecu[LG_MESSAGE]; /* le message de la couche Application ! */
+	int ecrits, lus; /* nb d’octets ecrits et lus */
+
+
+	memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
+	memset(messageRecu, 0x00, LG_MESSAGE*sizeof(char));
+
+	for(int i=0; i!=nbClient +1; i++){
+		//if( poll(&poll_message[1] , 1, 100) == 1 ) printf("teg\n");
+		if( poll(&poll_message[i], 1, 100) == 1 ){
+			lus = read(poll_message[i].fd, messageRecu, LG_MESSAGE*sizeof(char)); // ici appel bloquant
+			switch(lus)
+			{
+				case -1 : // une erreur ! 
+					perror("read");
+					close(poll_message[i].fd);
+					exit(-5);
+				case 0 : // la socket est fermée 
+					fprintf(stderr, "La socket a été fermée par le client !\n\n");
+					close(poll_message[i].fd);
+					//return 0;
+				default: printf("Message reçu du client : %s (%d octets)\n\n", messageRecu, lus);
+					
+			}
+			// On envoie des données vers le client (cf. protocole)
+			sprintf(messageEnvoi, "message reçu\n");
+			ecrits = write(poll_message[i].fd, messageEnvoi, strlen(messageEnvoi));
+			switch(ecrits)
+			{
+				case -1 : // une erreur ! 
+					perror("write");
+					close(poll_message[i].fd);
+					
+					exit(-6);
+				case 0 : // la socket est fermée 
+					fprintf(stderr, "La socket a été fermée par le client !\n\n");
+					close(poll_message[i].fd);
+					//return 0;
+				default: // envoi de n octets 
+					printf("Message %s envoyé (%d octets)\n\n", messageEnvoi, ecrits);
+			}
+		
+		
+		}
+	
+	}
+
+}
+
 int main(int nbArgs, char *args[]){
 	
 	/*recupère les paramètres pour initialiser le serveur*/
 	Parametre parametres;
 	parametres = parametrage(nbArgs, args);
-	printf("%d\n", parametres.PORT);
-	printf("%d\n", parametres.dimensions[0]);
 	/*--------------------------------------------------*/
-	
-	User *tete_liste = NULL;
 	
 	int socketEcoute;
 	struct sockaddr_in pointDeRencontreLocal;
@@ -140,29 +196,41 @@ int main(int nbArgs, char *args[]){
 	printf("Socket placée en écoute passive ...\n");
 	
 	
-	/*creation d'un pollfd pour l'acceuil de plusieur clients*/
+	/*creation d'un pollfd pour l'acceuil de plusieur clients*, et d'un tableau de poll pour la reception des différents message*/
 	struct pollfd poll_accept;
 	memset(&poll_accept, 0, sizeof(poll_accept));
 	poll_accept.fd = socketEcoute;
 	poll_accept.events = POLLIN; 
-	/*------------------------------------------------------*/
+	
+	struct pollfd *poll_message = NULL;
+	/*-------------------------------------------------------------------------------------------------------------------------*/
 	
 	
 	memset(messageEnvoi, 0x00, LG_MESSAGE*sizeof(char));
 	memset(messageRecu, 0x00, LG_MESSAGE*sizeof(char));
 	printf("Attente d’une demande de connexion (quitter avec Ctrl-C)\n\n");
 	
-	// boucle d’attente de connexion : en théorie, un serveur attend indéfiniment !
+	printf("PORT : %d\n", parametres.PORT);
+	printf("LARGEUR : %d\n", parametres.dimensions[0]);
+	printf("HAUTEUR : %d\n", parametres.dimensions[1]);
+	printf("LIMITE DE PIXELS : %d\n\n", parametres.limite);
+	
+	
+	User *tete_liste = NULL;
+	int nbClient = 0;
 	while(1)
 	{	
 		/*----------------------------poll---------------------------------------------------------*/
+		
 		if( poll(&poll_accept, 1, 100) == 1){
 		
 			//socketDialogue = accept(socketEcoute, (struct sockaddr *)&pointDeRencontreDistant, &longueurAdresse);
 			tete_liste = ajout_Client(tete_liste, socketEcoute, &longueurAdresse);
 			
 			User *parcour = tete_liste;
-			while(parcour->suivant != NULL) parcour = parcour->suivant;
+			while(parcour->suivant != NULL){
+				parcour = parcour->suivant;
+			}
 			
 			if (parcour->socketClient < 0)
 			{
@@ -173,57 +241,27 @@ int main(int nbArgs, char *args[]){
 			}
 			// On réceptionne les données du client et on affiche ses informations
 			printf("Connection client de : %s:%d\n", inet_ntoa(parcour->sockin->sin_addr),ntohs(parcour->sockin->sin_port));
-			free(parcour);
+			printf("%d\n", parcour->socketClient);
 			
+			nbClient +=1 ;
+			poll_message = (struct pollfd*) realloc(poll_message, nbClient);
+			
+			poll_message[nbClient].fd = parcour->socketClient;
+			poll_message[nbClient].events = POLLIN;
+			
+			printf("%d\n", poll_message[1].fd );
+			
+			free(parcour);
 		}
 		
-		/*creation d'un pollfd pour la reception de message de clients
-		struct pollfd poll_message;
-		memset(&poll_message, 0, sizeof(poll_message));
-		poll_message.fd = socketDialogue;
-		poll_message.events = POLLIN; 
-		/------------------------------------------------------*/
 		
-		/*
-		if(poll(&poll_message, 1, 100) == 1){			
-			lus = read(socketDialogue, messageRecu, LG_MESSAGE*sizeof(char)); // ici appel bloquant
-			switch(lus)
-			{
-				case -1 : // une erreur ! 
-					perror("read");
-					close(socketDialogue);
-					exit(-5);
-				case 0 : // la socket est fermée 
-					fprintf(stderr, "La socket a été fermée par le client !\n\n");
-					close(socketDialogue);
-					return 0;
-				default: printf("Message reçu du client : %s (%d octets)\n\n", messageRecu, lus);
-					
-			}
-			// On envoie des données vers le client (cf. protocole)
-			sprintf(messageEnvoi, "message reçu\n");
-			ecrits = write(socketDialogue, messageEnvoi, strlen(messageEnvoi));
-			switch(ecrits)
-			{
-				case -1 : // une erreur ! 
-					perror("write");
-					close(socketDialogue);
-					
-					exit(-6);
-				case 0 : // la socket est fermée 
-					fprintf(stderr, "La socket a été fermée par le client !\n\n");
-					close(socketDialogue);
-					return 0;
-				default: // envoi de n octets 
-					printf("Message %s envoyé (%d octets)\n\n", messageEnvoi, ecrits);
-			}			
-		}*/
+		reception_message(poll_message, nbClient);
+
 		/*-----------------------------------------------------------------------------------*/
 		
 		// On ferme la socket de dialogue et on se replace en attente ..
-		//close(socketDialogue); //pas forcement 
-	//else printf("test\n");
+		//close(socketDialogue); //faire en sorte de toute les fermer
 	}
-	//close(socketEcoute);
+	close(socketEcoute);
 	return 0;
 }
